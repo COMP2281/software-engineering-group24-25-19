136004a7ca4b62a9abc873d9de10a6eb381e3986
pawelliskiewicz <pawelliskiewicz04@gmail.com>
2024-12-06 11:37:15 +0000

    Basic table functionality

--- durmetrics/src/components/Content.js
+++ durmetrics/src/components/Content.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import DataTable from './DataTable';
 
 const Content = () => {
         return (
-                <>Content</>
+                <DataTable />
         );
 };
 
