6a70b223f7477d60826bca1dcdfe2dbea834fcd8
Henry Hudson <henry.b.hudson@durham.ac.uk>
2024-12-06 12:09:57 +0000

    Content layout

--- durmetrics/src/components/Content.js
+++ durmetrics/src/components/Content.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import DataTable from './DataTable';
+// import SearchBar from './SearchBar';
+import Dropdown from './Dropdown';
 
 const Content = () => {
         return (
-                <DataTable />
+                <div className="body">
+                        <div class="table-filters">
+                                <div class="input-group">
+                                        <div class="search-icon-container">
+                                                <img class="search-icon" src="search-icon.svg" />
+                                        </div>
+                                        <input class="search-bar" placeholder="Search sites by name or code..." />
+                                </div>
+                                <div class="years-dropdown">
+                                        Display Years
+                                        <img src="arrow-down.svg" class="dropdown-icon" />
+                                </div>
+                        </div>
+                        <div class="table-container">
+                                <DataTable />
+                        </div>
+                </div>
         );
 };
 
