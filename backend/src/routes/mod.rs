38fdad8e1f449f3e1d963096aa8115170a9f1b80
sglre6355 <sglre6355@gmail.com>
2025-01-25 04:58:31 +0000

    feat(backend): add `/sites` endpoint

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,5 +1,6 @@
 pub mod electricity_usage;
 pub mod gas_usage;
+pub mod sites;
 pub mod upload;
 
 use crate::structs::AppState;
@@ -8,6 +9,7 @@ use std::sync::Arc;
 
 pub fn create_router(shared_state: Arc<AppState>) -> Router {
     Router::new()
+        .nest("/sites", sites::router())
         .nest("/upload", upload::router())
         .nest("/gas-usage", gas_usage::router())
         .nest("/electricity-usage", electricity_usage::router())
---
+++ backend/src/routes/sites.rs
@@ -0,0 +1,37 @@
+use crate::entities::site;
+use crate::structs::{ApiError, AppState};
+use axum::{extract::State, routing::get, Json, Router};
+use axum_extra::extract::Query;
+use sea_orm::{ColumnTrait as _, Condition, EntityTrait as _, QueryFilter as _};
+use serde::Deserialize;
+use std::sync::Arc;
+use tracing::debug;
+
+#[derive(Deserialize)]
+struct SitesParams {
+    #[serde(default)]
+    names: Vec<String>,
+}
+
+async fn sites(
+    State(state): State<Arc<AppState>>,
+    Query(params): Query<SitesParams>,
+) -> Result<Json<Vec<site::Model>>, ApiError> {
+    debug!("params.names={:?}", params.names);
+
+    let conditions = Condition::all().add_option(match params.names.is_empty() {
+        true => None,
+        false => Some(site::Column::Name.is_in(params.names)),
+    });
+
+    let matched_records: Vec<site::Model> = site::Entity::find()
+        .filter(conditions)
+        .all(&state.database_connection)
+        .await?;
+
+    Ok(Json(matched_records))
+}
+
+pub fn router() -> Router<Arc<AppState>> {
+    Router::new().route("/", get(sites))
+}
