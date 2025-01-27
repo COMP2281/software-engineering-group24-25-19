0ccefa5aa31993f5166eeefb89f405613fb44f19
sglre6355 <sglre6355@gmail.com>
2025-01-27 12:30:06 +0000

    refactor(backend): sort router nestings

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -10,10 +10,10 @@ use std::sync::Arc;
 
 pub fn create_router(shared_state: Arc<AppState>) -> Router {
     Router::new()
-        .nest("/sites", sites::router())
-        .nest("/upload", upload::router())
-        .nest("/gas-usage", gas_usage::router())
         .nest("/electricity-usage", electricity_usage::router())
         .nest("/emission-factors", emission_factors::router())
+        .nest("/gas-usage", gas_usage::router())
+        .nest("/sites", sites::router())
+        .nest("/upload", upload::router())
         .with_state(shared_state)
 }
