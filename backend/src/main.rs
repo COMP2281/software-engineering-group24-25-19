7f0b2c65e28d94281817744370919d9ad9230528
sglre6355 <sglre6355@gmail.com>
2025-02-08 02:48:49 +0000

    feat(backend): update CORS policy to allow all HTTP methods

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,7 +7,7 @@ use sea_orm::Database;
 use std::env;
 use std::sync::Arc;
 use structs::AppState;
-use tower_http::cors::{AllowOrigin, CorsLayer};
+use tower_http::cors::{AllowMethods, AllowOrigin, CorsLayer};
 use tracing::{info, instrument};
 
 #[tokio::main]
@@ -23,8 +23,11 @@ async fn main() -> Result<()> {
         database_connection,
     });
 
-    let app = routes::create_router(shared_state)
-        .layer(CorsLayer::new().allow_origin(AllowOrigin::any()));
+    let app = routes::create_router(shared_state).layer(
+        CorsLayer::new()
+            .allow_methods(AllowMethods::any())
+            .allow_origin(AllowOrigin::any()),
+    );
 
     // TODO: make binding address configurable
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await?;
