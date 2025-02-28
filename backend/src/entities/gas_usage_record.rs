c35f61865982cf97390baeef5f4c5513558d07bd
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-02-28 12:38:38 +0000

    comments on gas_usage_records

--- backend/src/entities/gas_usage_record.rs
+++ backend/src/entities/gas_usage_record.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 
 #[derive(Clone, Debug, Default, PartialEq, DeriveEntityModel, Serialize)]
 #[sea_orm(table_name = "gas_usage_record")]
+// define the table name in the database
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
@@ -13,8 +14,18 @@ pub struct Model {
     pub cost_gbp: Option<f64>,
 }
 
+// primary key in 32-bit integer
+// site_id in 32-bit integer
+// start_year in 32-bit integer
+// end_year in 32-bit integer
+// energy_usage_kwh in 32-bit integer
+// cost_gbp in 64-bit float
+
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+
+// define an enum for relations
 pub enum Relation {
+    // define a relation to the site entity (foreign key)
     #[sea_orm(
         belongs_to = "super::site::Entity",
         from = "Column::SiteId",
@@ -23,10 +34,12 @@ pub enum Relation {
     Site,
 }
 
+// implement the Related trait for the Entity struct
 impl Related<super::site::Entity> for Entity {
     fn to() -> RelationDef {
         Relation::Site.def()
     }
 }
 
+// implement default bahaviour for the Model struct
 impl ActiveModelBehavior for ActiveModel {}
