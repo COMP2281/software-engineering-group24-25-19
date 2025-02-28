fdc4d9b3c85a3b1cd1c699bd82ef412afb829582
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-02-28 12:43:31 +0000

    comments on site.rs

--- backend/src/entities/site.rs
+++ backend/src/entities/site.rs
@@ -3,21 +3,40 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
 #[sea_orm(table_name = "site")]
+// defining site table
+
 pub struct Model {
     #[sea_orm(primary_key)]
+    // set id as primary keu
+
     #[serde(skip_deserializing)]
+    // skips deserialising during JSON parsing
+
     pub id: i32,
+    // id in 32-bit integer
+
     pub name: String,
+    // name in string
+    
     pub floor_area_square_metre: Option<f64>,
+    // floor_area_square_metre in 64-bit float, optional: can be null
+
     pub unique_property_reference_number: Option<String>,
+    // unique_property_reference_number in string, optional: can be null
+
     pub ni185_energy_user: Option<String>,
+    // ni185_energy_user in string, optional: can be null
+
     pub comment: Option<String>,
+    // comment in string, optional: can be null
 }
+
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
     #[sea_orm(has_many = "super::electricity_usage_record::Entity")]
     ElectricityUsageRecord,
+    // defining a one to many relationship with electricity_usage_record entity
 }
 
 impl Related<super::electricity_usage_record::Entity> for Entity {
@@ -25,5 +44,8 @@ impl Related<super::electricity_usage_record::Entity> for Entity {
         Relation::ElectricityUsageRecord.def()
     }
 }
+// implement the one to many relationship with electricity_usage_record entity
 
 impl ActiveModelBehavior for ActiveModel {}
+
+// implement default bahaviour for the Model struct
