a4c888df2f3947a6be41fae738533fbd222be330
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-07 12:53:23 +0000

    comments on site.rs

--- backend/src/entities/site.rs
+++ backend/src/entities/site.rs
@@ -12,18 +12,26 @@ pub struct Model {
     pub unique_property_reference_number: Option<String>,
     pub ni185_energy_user: Option<String>,
     pub comment: Option<String>,
+    //id in 32-bit integer and as primary key
+    //name in string
+    //floor_area_square_metre in 64-bit float
+    //unique_property_reference_number in string
+    //ni185_energy_user in string
+    //comment in string
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+// derive the EnumIter and DeriveRelation traits for the Relation enum
 pub enum Relation {
     #[sea_orm(has_many = "super::electricity_usage_record::Entity")]
     ElectricityUsageRecord,
 }
-
+// implement the ActiveModelBehavior trait for the Model struct
 impl Related<super::electricity_usage_record::Entity> for Entity {
+    // define the relation between the site table and the electricity_usage_record table
     fn to() -> RelationDef {
         Relation::ElectricityUsageRecord.def()
     }
 }
-
+// implement default bahaviour for the Model struct
 impl ActiveModelBehavior for ActiveModel {}
