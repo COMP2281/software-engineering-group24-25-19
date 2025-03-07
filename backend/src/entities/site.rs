658bff49b8d867359957dad4363e2a09884315e4
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-07 11:31:09 +0000

    fixing comments on mod.rs and site.rs

--- backend/src/entities/site.rs
+++ backend/src/entities/site.rs
@@ -8,29 +8,21 @@ use serde::{Deserialize, Serialize};
 pub struct Model {
     #[sea_orm(primary_key)]
     // set id as primary keu
-
     #[serde(skip_deserializing)]
     // skips deserialising during JSON parsing
-
     pub id: i32,
     // id in 32-bit integer
-
     pub name: String,
     // name in string
-    
     pub floor_area_square_metre: Option<f64>,
     // floor_area_square_metre in 64-bit float, optional: can be null
-
     pub unique_property_reference_number: Option<String>,
     // unique_property_reference_number in string, optional: can be null
-
     pub ni185_energy_user: Option<String>,
     // ni185_energy_user in string, optional: can be null
-
     pub comment: Option<String>,
     // comment in string, optional: can be null
 }
-
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
--- backend/src/routes/electricity_usage/records/mod.rs
+++ backend/src/routes/electricity_usage/records/mod.rs
@@ -4,6 +4,8 @@ use crate::app_state::AppState;
 use axum::{routing, Router};
 use std::sync::Arc;
 
+// creates a router for the electricity-usage module
 pub(super) fn router() -> Router<Arc<AppState>> {
     Router::new().route("/", routing::get(get::handler))
+    // returns a router instance with a route for GET requests at the root path "/"
 }
