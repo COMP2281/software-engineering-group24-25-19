43b2dcb72f63bbd87ddf6bf9e748e6767ccbd093
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-10 15:09:03 +0000

    comments on get.rs

--- backend/src/routes/gas_usage/records/get.rs
+++ backend/src/routes/gas_usage/records/get.rs
@@ -10,18 +10,26 @@ use serde::Deserialize;
 use std::sync::Arc;
 use tracing::debug;
 
+// Define a struct that represents the query parameters
+// site_ids in 32-bit integer format
+// start_years in 32-bit integer format
+// optional fields apply to the query parameters
 #[derive(Debug, Deserialize)]
 pub(super) struct QueryParams {
     site_ids: Option<Vec<i32>>,
     start_years: Option<Vec<i32>>,
 }
 
+// handler function for the endpoint
 pub(super) async fn handler(
+    // state: application state
     State(state): State<Arc<AppState>>,
+    // query_params: query parameters
     Query(query_params): Query<QueryParams>,
 ) -> Result<(StatusCode, Json<Vec<gas_usage_record::Model>>), ApiError> {
     debug!("query_params = {:?}", query_params);
-
+    // log the query parameters
+    // build the query conditions based on the query parameters
     let conditions = Condition::all()
         .add_option(
             query_params
@@ -33,11 +41,11 @@ pub(super) async fn handler(
                 .start_years
                 .map(|start_years| gas_usage_record::Column::StartYear.is_in(start_years)),
         );
-
+    //execute the query to find all gas usage records that match the conditions
     let matched_records: Vec<gas_usage_record::Model> = gas_usage_record::Entity::find()
         .filter(conditions)
         .all(&state.database_connection)
         .await?;
-
+    // return the matched records as a JSON response
     Ok((StatusCode::OK, Json(matched_records)))
 }
