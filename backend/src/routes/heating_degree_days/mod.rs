faf6f776f693cedd516f5930b5fe9d2cb2ca10a5
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-10 15:58:29 +0000

    comment

--- backend/src/routes/heating_degree_days/mod.rs
+++ backend/src/routes/heating_degree_days/mod.rs
@@ -6,4 +6,6 @@ use std::sync::Arc;
 
 pub(super) fn router() -> Router<Arc<AppState>> {
     Router::new().route("/", routing::get(get::handler))
+    // a nedted route for handling gas usage records
+    // router creates a new Router instance and nests the /records route
 }
