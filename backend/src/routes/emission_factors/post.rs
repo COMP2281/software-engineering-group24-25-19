89f37139db21f67f925610e61cc84fbfe1cbc7af
Henry Hudson <henry.b.hudson@durham.ac.uk>
2025-03-14 06:35:40 +0000

    fix(backend): handle `emission-factors` case where entry is empty

--- backend/src/routes/emission_factors/post.rs
+++ backend/src/routes/emission_factors/post.rs
@@ -16,8 +16,8 @@ use tracing::debug;
 #[derive(Debug, Default, Deserialize)]
 pub(super) struct Payload {
     pub start_year: i32,
-    pub gas: f64,
-    pub electricity: f64,
+    pub gas: Option<f64>,
+    pub electricity: Option<f64>,
 }
 
 /// Handles POST requests to create a new site
@@ -31,6 +31,9 @@ pub(super) async fn handler(
 
     let db = &state.database_connection;
 
+    let gas = payload.gas.unwrap_or(0.0);
+    let electricity = payload.electricity.unwrap_or(0.0);
+
     // Try to find an existing emission factor with given start_year
     if let Some(existing_emission_factor) = emission_factor::Entity::find()
         .filter(emission_factor::Column::StartYear.eq(payload.start_year))
@@ -39,8 +42,8 @@ pub(super) async fn handler(
     {
         // Update the existing record
         let mut active_model = existing_emission_factor.into_active_model();
-        active_model.gas = Set(payload.gas);
-        active_model.electricity = Set(payload.electricity);
+        active_model.gas = Set(gas);
+        active_model.electricity = Set(electricity);
 
         let updated_model = active_model.update(db).await?;
         Ok((StatusCode::OK, Json(updated_model)))
@@ -49,8 +52,8 @@ pub(super) async fn handler(
         let emission_factor_model = emission_factor::ActiveModel {
             start_year: Set(payload.start_year),
             end_year: NotSet,
-            gas: Set(payload.gas),
-            electricity: Set(payload.electricity),
+            gas: Set(gas),
+            electricity: Set(electricity),
         };
 
         let new_model = emission_factor_model.insert(db).await?;
