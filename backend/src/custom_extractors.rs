b25f309b4fdcb28457ea66be3d7cc3fd78d50bc4
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-14 12:39:46 +0000

    comments on custom_extractors.rs

--- backend/src/custom_extractors.rs
+++ backend/src/custom_extractors.rs
@@ -7,16 +7,21 @@ use axum::{
 use serde::de::DeserializeOwned;
 
 #[derive(Debug, FromRequest)]
+// derive the FromRequest trait for the Path struct
 #[from_request(via(axum::extract::Path), rejection(ApiError))]
+// specify the Path extractor and the rejection type as ApiError
 pub struct Path<T>(pub T);
-
+// define the Path struct with a generic type T
 impl<S, T> FromRequestParts<S> for Path<T>
+// implement the FromRequestParts trait for the Path struct
 where
     T: DeserializeOwned + Send,
     S: Send + Sync,
+    // specify the generic types T and S
 {
     type Rejection = ApiError;
-
+    // define the associated type Rejection as ApiError
+    // async function from_request_parts receives a mutable reference to Parts and a reference to S
     async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, ApiError> {
         match axum::extract::Path::<T>::from_request_parts(parts, state).await {
             Ok(value) => Ok(Self(value.0)),
