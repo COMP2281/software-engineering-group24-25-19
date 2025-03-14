aa932ce0d75a10e4aaccd9b5d1a4b916e0702f85
TiggieF <147111245+TiggieF@users.noreply.github.com>
2025-03-14 11:52:27 +0000

    comments on post.rs login

--- backend/src/routes/auth/login/post.rs
+++ backend/src/routes/auth/login/post.rs
@@ -8,8 +8,10 @@ use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
+// deserialize the request payload
 #[derive(Deserialize)]
 pub struct LoginRequest {
+    // login resquest contains a code in string format
     code: String,
 }
 
@@ -17,6 +19,8 @@ pub struct LoginRequest {
 pub struct LoginResponse {
     access_level: u8,
     token: Option<String>,
+    // login response contains an access level (u8)
+    // and a token (optional and string)
 }
 
 #[derive(Serialize)]
@@ -24,29 +28,37 @@ pub struct Claims {
     sub: String,
     exp: usize,
     access_level: u8,
+    // claims contains a subject (sub) which is a string
+    // an expiration time (exp) which is a usize
+    // and an access level which is a u8
 }
 
 pub(super) async fn handler(
+    // handler function receives a state of type State<AppState> and a payload of type Json<LoginRequest>
     State(state): State<Arc<AppState>>,
     Json(payload): Json<LoginRequest>,
 ) -> Json<LoginResponse> {
     let hashed_code = format!("{:x}", compute(payload.code.as_bytes()));
-
+    // hash the code using md5 and format it as a string
     let result = access_code::Entity::find()
         .filter(access_code::Column::HashedCode.eq(hashed_code.clone()))
         .one(&state.database_connection)
         .await
         .ok()
         .flatten();
+        // find the access code in the database using the hashed code and return the result
 
+    // if the result is found, create a token with the hashed code as the subject
     if let Some(entry) = result {
         let access_level = entry.access_level as u8;
         let token = if access_level > 0 {
             let claims = Claims {
                 sub: hashed_code.clone(),
                 exp: chrono::Utc::now().timestamp() as usize + 3600,
+                // a time allowance of 1 hour
                 access_level,
             };
+            // match the token creation and return the token if it is created successfully
             match encode(
                 &Header::default(),
                 &claims,
@@ -63,10 +75,12 @@ pub(super) async fn handler(
             access_level,
             token,
         });
+        // return the access level and the token
     }
 
     Json(LoginResponse {
         access_level: 0,
         token: None,
+        // return access level 0 and no token
     })
 }
