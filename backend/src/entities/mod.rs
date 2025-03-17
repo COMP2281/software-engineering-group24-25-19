7adb77469a4849f816e9ba658230b37b14eb045e
mobiusw <danielsheng33@gmail.com>
2025-03-18 06:19:03 +0800

    comments on mod.rs

--- backend/src/entities/mod.rs
+++ backend/src/entities/mod.rs
@@ -1,6 +1,15 @@
+// This module serves as an entry point to re-export all entities within the `entities` directory
+// It allows other modules in this project to access database models easily
+
+/// Import and expose the `access_code` entity module
 pub mod access_code;
+/// Import and expose the `electricity_usage_record` entity module
 pub mod electricity_usage_record;
+/// Import and expose the `emission_factor` entity module
 pub mod emission_factor;
+/// Import and expose the `gas_usage_record` entity module
 pub mod gas_usage_record;
+/// Import and expose the `heating_degree_day` entity module
 pub mod heating_degree_day;
+/// Import and expose the `site` entity module
 pub mod site;
